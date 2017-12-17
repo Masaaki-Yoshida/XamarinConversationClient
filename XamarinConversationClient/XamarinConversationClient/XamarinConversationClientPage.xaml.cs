7021b9b7b871c178fd66f3bfc6d030278c301607
Masaaki Yoshida <ma-yoshida@cresco.co.jp>
2017-12-17 13:53:52 +0900

    change project to .net standard 2.0

---
+++ XamarinConversationClient/XamarinConversationClient/XamarinConversationClientPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinConversationClient
+{
+    public partial class XamarinConversationClientPage : ContentPage
+    {
+        public XamarinConversationClientPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
