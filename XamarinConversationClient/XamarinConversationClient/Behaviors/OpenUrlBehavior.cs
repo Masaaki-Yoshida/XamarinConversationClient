32edf15ddfa5fa9d6fd955962b92a546249e35ef
Masaaki Yoshida <masamba@MasaMBA-no-MacBook-Air.local>
2018-01-21 18:17:25 +0900

    Add Border Effect Add Open Ur lBehavior Add Xaml Image Resource Extension Add Json.Net Reference

---
+++ XamarinConversationClient/XamarinConversationClient/Behaviors/OpenUrlBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Prism.Behaviors;
+using Xamarin.Forms;
+
+namespace XamarinConversationClient.Behaviors
+{
+    public class OpenUrlBehavior : BehaviorBase<View>
+    {
+        public static readonly BindableProperty TargetProperty =
+            BindableProperty.Create("Target", typeof(object), typeof(OpenUrlBehavior));
+
+        public object TargetParameter
+        {
+            get { return GetValue(TargetProperty); }
+            set { SetValue(TargetProperty, value); }
+        }
+
+        protected override void OnAttachedTo(View bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            var gesture = new TapGestureRecognizer();
+            gesture.Tapped += bindable_Clicked;
+            bindable.GestureRecognizers.Add(gesture);
+        }
+
+        protected override void OnDetachingFrom(View bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.GestureRecognizers.Clear();
+        }
+
+        void bindable_Clicked(object sender, EventArgs e)
+        {
+            Uri uri;
+            var param = this.TargetParameter?.ToString();
+            if (Uri.TryCreate(param, UriKind.RelativeOrAbsolute, out uri))
+            {
+                Device.OpenUri(uri);
+            }
+        }
+    }
+}
